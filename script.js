3db11ce00112ee91e2faf8fe32b4c09335324cc7
Suryansh Pandey <suryanshpsurya@gmail.com>
2021-07-30 15:42:37 +0530

    update files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var taskbar = document.getElementsByClassName("taskbar")[0];
+var start = document.getElementsByClassName("start")[0];
 var startmenu = document.getElementsByClassName("startmenu")[0];
 
-taskbar.addEventListener("click", () => {
+start.addEventListener("click", () => {
  if(startmenu.style.bottom == "50px"){
   startmenu.style.bottom = "-550px"
  }
@@ -9,3 +9,27 @@ taskbar.addEventListener("click", () => {
   startmenu.style.bottom = "50px"
  }
 })
+
+let toggle_btn = document.querySelectorAll('.apps-icon');
+
+for (const toggle_btns of toggle_btn) {
+ toggle_btns.addEventListener("click", () => {
+  let submenu_toggle = document.querySelectorAll('.app');
+
+  for (let submenu_toggles of submenu_toggle) {
+
+   /* display the content if the value in the data attribute of the button and content are the same */
+   if (submenu_toggles.getAttribute('data-number') === toggle_btns.getAttribute('data-number')) {
+    submenu_toggles.classList.toggle('change');
+    toggle_btns.classList.toggle('change');
+   }
+
+   // if it's not equal then hide it.
+   else {
+    // submenu_toggles.classList.remove('change');
+   }
+
+  }
+ })
+}
+ 
